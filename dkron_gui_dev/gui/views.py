9f16b5c38c4c840dd71f290e20578a17771e387a
ropr123 <m.rohit.prabhu@gmail.com>
2021-03-18 18:54:57 +0530

    django-dkron integration

---
+++ dkron_gui_dev/gui/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+#from pydkron import DkronClient
+
+
+
+# Create your views here.
+
+def home(request):
+    return render(request,"index.html")
+
+def status(request):
+    return render(request,"status.html")
+
+def schedule(request):
+    Id=request.POST.get("id",False)
+    Name=request.POST.get("name",False)
+    DispName=request.POST.get("disp",False)
+    TimeZone=request.POST.get("tz",False)
+    Schedule=request.POST.get("sch",False)  
+    Owner=request.POST.get("own",False)
+    Owner_email=request.POST.get("ownem",False)
+    Parent_job=request.POST.get("pj",False)
+    Concur=request.POST.get("conc",False)
