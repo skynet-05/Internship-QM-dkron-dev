ee36e0967b1c89970534cb2eaec4c9272046ab92
skynet-05 <rahulreddy19981998@gmail.com>
2021-04-01 16:39:23 +0530

    Updated create files

--- dkron_gui_dev/static/js/create.js
+++ dkron_gui_dev/static/js/create.js
@@ -1,21 +1,50 @@
 function tpo() {
-    downpp.style.display = "block"
+    downpp.style.display = "inline"
     domm.style.display = "none"
     forminn.style.display = "none"
+    forhour.style.display = "none"
+    scheduleford.style.display = "none"
+    schedulefort.style.display = "inline"
 }
 
 function tporemove() {
     downpp.style.display = "none"
     domm.style.display = "none"
     forminn.style.display = "none"
+    forhour.style.display = "none"
+    scheduleford.style.display = "inline"
+    schedulefort.style.display = "inline"
+}
+
+function tporemovedaily() {
+    downpp.style.display = "none"
+    domm.style.display = "none"
+    forminn.style.display = "none"
+    forhour.style.display = "none"
+    scheduleford.style.display = "none"
+    schedulefort.style.display = "inline"
 }
 function tporemovemin() {
     downpp.style.display = "none"
     domm.style.display = "none"
-    forminn.style.display = "block"
+    forminn.style.display = "inline"
+    forhour.style.display = "none"
+    scheduleford.style.display = "none"
+    schedulefort.style.display = "none"
 }
 function tporemovemonth() {
     downpp.style.display = "none"
-    domm.style.display = "block"
+    domm.style.display = "inline"
     forminn.style.display = "none"
+    forhour.style.display = "none"
+    scheduleford.style.display = "none"
+    schedulefort.style.display = "inline"
+}
+function tporemovehour() {
+    downpp.style.display = "none"
+    domm.style.display = "none"
+    forminn.style.display = "none"
+    forhour.style.display = "inline"
+    scheduleford.style.display = "none"
+    schedulefort.style.display = "inline"
 }
