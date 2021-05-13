8d3af9d822d89a43a767a1e28a8659c9d70df04a
mayank98108 <68019387+mayank98108@users.noreply.github.com>
2021-05-13 08:08:42 +0530

    Update send_mail.py
    removed my gmail credentials and added input("") to get it from the user

--- send_mail.py
+++ send_mail.py
@@ -4,9 +4,9 @@ print("Note: For sending email you must have less secure app access enabled in y
 print("!Still working on to make this script work with good api instead.")
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "pushpa98108@gmail.com"  # Enter your address
-receiver_email = "mayank98108@gmail.com"  # Enter receiver address
-password = "!Qwertyuiop09"
+sender_email = input("Enter Email:")  # Enter your address
+receiver_email = input("Enter receiver email:")  # Enter receiver address
+password = input("Enter Password:")
 message = "testing dkron"
 
 context = ssl.create_default_context()
